ed45ae1359301d48f034944d4ebbebaa2e0a454c
DemianJG <115387167+DemianJG@users.noreply.github.com>
2022-11-02 12:12:58 -0600

    Actualización de rutas backend
    Se actualizaron las rutas del backend

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', 'LoginController@index')->name('login');
+Route::post('/login', 'LoginController@login')->name('login.perform');
+
+Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
